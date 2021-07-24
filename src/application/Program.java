4c4e9a3fc265fc2e9b83d09fc9fae9f1ac2736aa
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-07-24 15:34:44 -0300

    Camando o metodo que trabalha com listas tipo curinga apartir de uma lista de Strings

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,12 @@ public class Program {
 	public static void main(String[] args) {
 		List<Integer> myInts = Arrays.asList(5, 2, 10);
 		printList(myInts);
+		
+		System.out.println();
+		
+		List<String> myString = Arrays.asList("Maria", "Pedro", "João");
+		printList(myString);
+		
 	}
 	
 	public static void printList(List<?> list) {
